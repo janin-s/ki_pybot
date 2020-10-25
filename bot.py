bfef0e36039827350a2fc2d584605b5fed2c8436
janin <janin.chaib@tum.de>
2020-10-25 02:23:50 +0100

    defd

--- bot.py
+++ bot.py
@@ -165,13 +165,13 @@ async def react(ctx, reaction):
         unicode_id: str = unicode_id.upper()
 
         await ctx.send(unicode_id)
-        await ctx.message.add_reaction(str(unicode_id))
+        await ctx.message.add_reaction('\\U000{0}'.format(str(unicode_id)))
 
 
 async def getUnicodeId(c):
     id_dec: int = 127462 + ((ord(c) - 65) if c.isupper() else (ord(c) - 97))
     id_hex: str = hex(id_dec)[2:]
-    return "\U000" + id_hex
+    return id_hex
 
 
 async def areCharactersUnique(s):
