fea6a511d81443189e5132114639186e20cc4b57
severinsch <severin.schmidmeiers@gmail.com>
2020-10-26 14:01:31 +0100

    added punish

--- bot.py
+++ bot.py
@@ -6,22 +6,6 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
-shot_counter = 0
-
-letter_dict = {'A': '\U0001f1e6', 'B': '\U0001f1e7', 'C': '\U0001f1e8', 'D': '\U0001f1e9', 'E': '\U0001f1ea',
-               'F': '\U0001f1eb', 'G': '\U0001f1ec', 'H': '\U0001f1ed', 'I': '\U0001f1ee', 'J': '\U0001f1ef',
-               'K': '\U0001f1f0', 'L': '\U0001f1f1', 'M': '\U0001f1f2', 'N': '\U0001f1f3', 'O': '\U0001f1f4',
-               'P': '\U0001f1f5', 'Q': '\U0001f1f6', 'R': '\U0001f1f7', 'S': '\U0001f1f8', 'T': '\U0001f1f9',
-               'U': '\U0001f1fa', 'V': '\U0001f1fb', 'W': '\U0001f1fc', 'X': '\U0001f1fd', 'Y': '\U0001f1fe',
-               'Z': '\U0001f1ff', 'a': '\U0001f1e6', 'b': '\U0001f1e7', 'c': '\U0001f1e8', 'd': '\U0001f1e9',
-               'e': '\U0001f1ea', 'f': '\U0001f1eb', 'g': '\U0001f1ec', 'h': '\U0001f1ed', 'i': '\U0001f1ee',
-               'j': '\U0001f1ef', 'k': '\U0001f1f0', 'l': '\U0001f1f1', 'm': '\U0001f1f2', 'n': '\U0001f1f3',
-               'o': '\U0001f1f4', 'p': '\U0001f1f5', 'q': '\U0001f1f6', 'r': '\U0001f1f7', 's': '\U0001f1f8',
-               't': '\U0001f1f9', 'u': '\U0001f1fa', 'v': '\U0001f1fb', 'w': '\U0001f1fc', 'x': '\U0001f1fd',
-               'y': '\U0001f1fe', 'z': '\U0001f1ff', '0': '\U00000030\U000020E3', '1': '\U00000031\U000020E3',
-               '2': '\U00000032\U000020E3', '3': '\U00000033\U000020E3', '4': '\U00000034\U000020E3',
-               '5': '\U00000035\U000020E3', '6': '\U00000036\U000020E3', '7': '\U00000037\U000020E3',
-               '8': '\U00000038\U000020E3', '9': '\U00000039\U000020E3', '?': '\U00002753', '!': '\U00002757'}
 
 
 @bot.event
@@ -35,7 +19,7 @@ async def on_command_error(ctx, error, force=False):
     if isinstance(error, commands.errors.CommandNotFound):
         await ctx.send('KI dummdumm <:eist_moment:731293248324370483>')
     else:
-        await ctx.send("KI nix verstehi ._.")
+        await ctx.send("KI nix verstehi ._." + str(error))
 
 
 @bot.command()
@@ -276,6 +260,22 @@ def get_unicode_id(c):
     if o == 45:
         return '\U00002796'
     return '\U00002753'
+
+
+def punish(ctx):
+    user_list = ctx.message.mentions
+    for user in user_list:
+        dm_channel = user.dm_channel
+        invite = await ctx.channel.create_invite(max_uses=1)
+        if dm_channel is None:
+            dm_channel = await user.create_dm()
+        try:
+            await user.kick(reason="Bestrafung")
+        except discord.Forbidden:
+            await ctx.send("KI nicht mächtig genug")
+        for i in range(10):
+            await dm_channel.send("shame!")
+        await dm_channel.send(invite.url)
 
 
 bot.run('NzA5ODY1MjU1NDc5NjcyODYz.XrsH2Q.46qaDs7GDohafDcEe5Ruf5Y7oGY')
