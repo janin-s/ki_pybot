60ac7d38b20d9283a0377a2beb5cb9bc795edaa9
severinsch <severin.schmidmeiers@gmail.com>
2020-11-02 18:32:28 +0100

    fixed punish this time really

--- helper_functions.py
+++ helper_functions.py
@@ -98,10 +98,13 @@ def get_file(file: str):
 
 async def add_entry(file: str, key: Union[int, str], line: Union[int, str]):
     file_path = os.path.join(DIR, file)
-    with open(file_path, "r+") as f:
-        file_dict: dict = json.load(f)
-        file_dict[str(key)] = str(line)
-        json.dump(file_dict, f, separators=(',', ': '), indent=4)
+    with open(file_path, "r") as fr:
+        file_dict: dict = json.load(fr)
+
+    file_dict[str(key)] = str(line)
+
+    with open(file_path, "w") as fw:
+        json.dump(file_dict, fw, separators=(',', ': '), indent=4)
 
     return f"added line {line} with key {key} to {file}"
 
@@ -110,21 +113,26 @@ def get_entry(file: str, key: Union[int, str]):
     file_path = os.path.join(DIR, file)
     with open(file_path, "r+") as f:
         file_dict: dict = json.load(f)
-        try:
-            entry = file_dict[str(key)]
-        except KeyError:
-            entry = "no entry for this key"
-        output = (str(key), str(entry))
+
+    try:
+        entry = file_dict[str(key)]
+    except KeyError:
+        entry = "no entry for this key"
+
+    output = (str(key), str(entry))
     return output
 
 
 async def remove_entry(file: str, key: Union[int, str]):
     file_path = os.path.join(DIR, file)
-    with open(file_path, "r+") as f:
-        file_dict: dict = json.load(f)
-        try:
-            entry = file_dict.pop(str(key))
-        except KeyError:
-            entry = "not found"
-        json.dump(file_dict, f, separators=(',', ': '), indent=4)
+    with open(file_path, "r") as fr:
+        file_dict: dict = json.load(fr)
+
+    try:
+        entry = file_dict.pop(str(key))
+    except KeyError:
+        entry = "not found"
+
+    with open(file_path, "w") as fw:
+        json.dump(file_dict, fw, separators=(',', ': '), indent=4)
     return f"removed entry {entry} with key {key} from {file}"
