4fada8388218a081b9dc06681efd86630049b1e4
sev <severin.schmidmeier@tum.de>
2022-03-20 12:09:13 +0100

    somew changes to daily info

--- lib/cogs/daily_infos.py
+++ lib/cogs/daily_infos.py
@@ -36,13 +36,14 @@ class DailyInfos(Cog):
 
     async def print_daily_infos_for_guild(self, name: str, main_channel_id: int, quote_channel_id: int):
         main_channel: TextChannel = await self.bot.fetch_channel(main_channel_id)
+        quote_channel: TextChannel = await self.bot.fetch_channel(quote_channel_id)
 
         main_embed: Embed = Embed(title=f'Guten Morgen {name}')
         inzidenz_file = get_incidence_image_embed()
         main_embed.set_image(url='attachment://incidence.png')
         await main_channel.send(file=inzidenz_file, embed=main_embed)
 
-        relikte_embed = await self.get_relikte_throwback_embed(quote_channel_id)
+        relikte_embed = await get_relikte_throwback_embed(quote_channel)
         weather_embed = get_weather_info_embed(self.WEATHER_API_KEY)
         news_embed = get_news_embed(self.NEWS_API_KEY)
         embeds = [relikte_embed, weather_embed, news_embed]
@@ -50,33 +51,33 @@ class DailyInfos(Cog):
             if e is not None:
                 await main_channel.send(embed=e)
 
-    async def get_relikte_throwback_embed(self, quote_channel_id) -> Embed | None:
-        channel: TextChannel = await self.bot.fetch_channel(quote_channel_id)
-        if channel is None:
-            return
-        today = datetime.today()
-        # TODO handle leap year, where there is no history
-        old_messages = []
-        for date in [today.replace(year=today.year - i) for i in range(1, 5)]:
+
+async def get_relikte_throwback_embed(channel: TextChannel) -> Embed | None:
+    if channel is None:
+        return
+    today = datetime.today()
+    # TODO handle leap year, where there is no history
+    old_messages = []
+    for date in [today.replace(year=today.year - i) for i in range(1, 5)]:
         # for date in [today - timedelta(days=i) for i in range(1, 5)]:
-            history = channel.history(before=date + timedelta(days=1), after=date)
-            async for m in history:
-                old_messages.append(m)
+        history = channel.history(before=date + timedelta(days=1), after=date)
+        async for m in history:
+            old_messages.append(m)
 
-        if len(old_messages) == 0:
-            return None
+    if len(old_messages) == 0:
+        return None
 
-        old_message: Message = random.choice(old_messages)
-        year = datetime.today().year - old_message.created_at.year
+    old_message: Message = random.choice(old_messages)
+    year = datetime.today().year - old_message.created_at.year
 
-        info_message = f"{old_message.author.display_name} {year} year{'' if year == 1 else 's'} ago today:"
+    info_message = f"{old_message.author.display_name} {year} year{'' if year == 1 else 's'} ago today:"
 
-        if old_message.attachments is None or len(old_message.attachments) == 0:
-            embed: Embed = Embed(title=info_message, description=old_message.content)
-            return embed
-        embed: Embed = Embed(title=info_message)
-        embed.set_image(url=old_message.attachments[0].url)
+    if old_message.attachments is None or len(old_message.attachments) == 0:
+        embed: Embed = Embed(title=info_message, description=old_message.content)
         return embed
+    embed: Embed = Embed(title=info_message)
+    embed.set_image(url=old_message.attachments[0].url)
+    return embed
 
 
 def get_weather_info_embed(api_key) -> Embed | None:
