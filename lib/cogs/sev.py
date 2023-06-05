19d0b80ed414ac53c0bb2a48d0408cab8cf1d722
c-rotte <54217818+rchr1@users.noreply.github.com>
2023-06-05 22:06:07 +0200

    ~

--- lib/cogs/sev.py
+++ lib/cogs/sev.py
@@ -65,9 +65,9 @@ class Sev(Cog):
         if not any(user.id == self.sev_id for user in message.mentions):
             return
         # Only impersonate sev with a certain probability
-        if not np.random.choice(a=[True, False], p=[0.1, 0.9]):
-            print("sev mag net antworten")
-            return
+        #if not np.random.choice(a=[True, False], p=[0.2, 0.8]):
+        #    print("sev mag net antworten")
+        #    return
         try:
             bot_msg: str = self.generate_response(message=message.clean_content)
         except Exception as e:
