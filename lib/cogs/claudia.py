af1239ce3788de16249d5341d0883168910098f5
Janin Chaib <janin.chaib@tum.de>
2023-08-25 19:35:29 +0200

    Change claudia to webhook

--- lib/cogs/claudia.py
+++ lib/cogs/claudia.py
@@ -11,35 +11,74 @@ from lib.bot import Bot
 class Claudia(Cog):
     def __init__(self, bot):
         self.bot: Bot = bot
-        self.role_desc = Path("res/claudia_prompt.txt") \
-            .read_text() \
-            .replace("\n", " ") \
-            .strip()
+        self.role_desc = (
+            Path("res/claudia_prompt.txt").read_text().replace("\n", " ").strip()
+        )
 
+    @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("Claudia enabled!")
+            self.bot.cogs_ready.ready_up("Gottköniginclaudia ist auferstanden!")
 
-    @command()
-    async def claudia(self, ctx: Context, *, prompt):
+    async def send_message_as_claudia(
+        self, message: str, channel: TextChannel, guild: Guild, file
+    ):
+        guild_webhooks: list[discord.Webhook] = await guild.webhooks()
+        webhooks_filtered: list[discord.Webhook] = [
+            w for w in guild_webhooks if str(channel.id) in w.name
+        ]
+        if not webhooks_filtered:
+            webhook: discord.Webhook = await channel.create_webhook(
+                name=f"say-cmd-hook-{channel.id}"
+            )
+        else:
+            webhook: discord.Webhook = webhooks_filtered[0]
+        await webhook.send(
+            content=message,
+            username="Prof. Claudi",
+            avatar_url="https://www.acatech.de/wp-content/uploads/2021/06/Claudia-Eckert_Zuschnitt.jpg",
+            file=file,
+        )
 
+    @Cog.listener()
+    async def on_message(self, message: Message):
+        if not self.enabled:
+            return
+        if not any(
+            message.content.lower().startswith(
+                ("@Claudia ", "@Claudi ", "@Claudimausi ", "@Eggard ", "@Eggi ")
+            )
+        ):
+            return
+
+        guild = message.guild
+        channel = message.channel
         file_paths = []
-        for a in ctx.message.attachments:
+        for a in message.attachments:
             if a.content_type not in ["application/pdf", "text/plain"]:
-                return await ctx.reply("Please only upload PDF or TXT files.", mention_author=False)
+                return await self.send_message_as_claudia(
+                    "Als Professorin für Informatik an der TUM erwarte ich PDFs oder reinen Text. Ich bitte Sie höflichst, dass Sie sich an diese Vorgaben halten.",
+                    channel,
+                    guild,
+                    None,
+                )
             file_path = f"/tmp/{a.filename}"
             await a.save(file_path)
             file_paths.append(file_path)
 
-        response, costs = llm.claude(self.bot).get_response(self.role_desc, prompt, file_paths)
+        response, costs = llm.claude(self.bot).get_response(
+            self.role_desc, message.content.split(" ", 1)[1], file_paths
+        )
         if len(response) > 1000:
             with open("/tmp/claudia.txt", "w") as f:
                 f.write(response)
             message = f"||Diese Nachricht hat {costs:.2f}ct gekostet||"
-            return await ctx.reply(message, file=discord.File("/tmp/claudia.txt"), mention_author=False)
+            return await self.send_message_as_claudia(
+                message, channel, guild, file=discord.File("/tmp/claudia.txt")
+            )
         else:
             message = f"{response}\n\n||Diese Nachricht hat {costs:.2f}ct gekostet||"
-            return await ctx.reply(message, mention_author=False)
+            return await self.send_message_as_claudia(message, channel, guild, None)
 
 
 def setup(bot):
