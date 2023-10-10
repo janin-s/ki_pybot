2699ba3e0311e744ae21907da9a697dfdbb81cae
sev <severin.schmidmeier@tum.de>
2023-10-10 16:31:10 +0200

    added RSS feed feature

--- lib/cogs/rss_feeds.py
+++ lib/cogs/rss_feeds.py
@@ -9,14 +9,15 @@ import discord
 import feedparser
 from discord import Embed
 from discord.ext import commands, tasks
-from discord.ext.commands import Cog, command
+from discord.ext.commands import Cog, command, Context
 from feedparser import FeedParserDict
 from markdownify import markdownify as md
+from bs4 import BeautifulSoup
 
 from lib.db import db
 from lib.bot import Bot
 
-FeedId = Literal['fefe', 'schneier', 'jbl', 'kuketz']
+FeedId = Literal['fefe', 'schneier', 'jbl', 'kuketz', 'xkcd']
 
 
 @dataclasses.dataclass(frozen=True)
@@ -26,6 +27,7 @@ class FeedEntry:
     link: str | None
     published: datetime | None
     id: str | None
+    image: str | None = None
 
 
 @dataclasses.dataclass(frozen=True)
@@ -38,11 +40,18 @@ class RSSFeed:
     entries: list[FeedEntry]
 
 
+@dataclasses.dataclass(frozen=True)
+class Image:
+    url: str
+    title: str | None
+
+
 FEED_COLOURS: dict[FeedId, int] = {
     'fefe': 0xFFFFFF,
     'schneier': 0x6b0000,
     'jbl': 0x000000,
     'kuketz': 0x628098,
+    'xkcd': 0x96A8C8,
 }
 
 FEEDS: list[tuple[FeedId, str]] = [
@@ -50,11 +59,23 @@ FEEDS: list[tuple[FeedId, str]] = [
     ('schneier', 'https://www.schneier.com/feed/'),
     ('jbl', 'https://blog.cr.yp.to/feed.application=xml'),
     ('kuketz', 'https://www.kuketz-blog.de/feed/'),
+    ('xkcd', 'https://xkcd.com/rss.xml'),
 ]
 
 UPDATE_MINUTES = 10
 
 FEED_CHANNEL_ID = 1160283395717349406
+
+
+def extract_images(content: str) -> Image | None:
+    soup = BeautifulSoup(content, features='html.parser')
+    img = soup.find('img')
+    if not img:
+        return None
+    return Image(
+        url=img['src'],
+        title=img.get('title')
+    )
 
 
 def extract_data(feed_id: FeedId, result: FeedParserDict) -> RSSFeed:
@@ -71,7 +92,13 @@ def extract_data(feed_id: FeedId, result: FeedParserDict) -> RSSFeed:
         icon_url=icon_url
     )
     for e in result.entries:
-        content = md(e.summary) if 'summary' in e else None
+        image = extract_images(e.summary)
+        content = md(e.summary, strip=['img']) if 'summary' in e else None
+        if not content:
+            if image:
+                content = image.title
+            else:
+                continue
         published_time = datetime.fromtimestamp(mktime(e.published_parsed)) if 'published_parsed' in e else None
         feed.entries.append(
             FeedEntry(
@@ -80,6 +107,7 @@ def extract_data(feed_id: FeedId, result: FeedParserDict) -> RSSFeed:
                 link=e.link if 'link' in e else None,
                 published=published_time,
                 id=e.id if 'id' in e else None,
+                image=image.url if image else None
             )
         )
     return feed
@@ -98,6 +126,9 @@ def format_entry(feed: RSSFeed, entry: FeedEntry) -> Embed:
                      )
 
     embed.set_footer(text=feed.title)
+
+    if entry.image:
+        embed.set_image(url=entry.image)
     return embed
 
 
@@ -117,8 +148,8 @@ class RSS(Cog):
 
     @command()
     @commands.has_permissions(administrator=True)
-    async def get_feeds(self, _):
-        await self.rss_update_loop()
+    async def get_feeds(self, ctx: Context):
+        await self.rss_update_loop(channel_id=ctx.channel.id)
 
     def get_new_entries(self, feed: RSSFeed) -> Iterable[FeedEntry]:
         if feed.updated and feed.updated > (datetime.now() - timedelta(minutes=UPDATE_MINUTES)):
@@ -134,8 +165,8 @@ class RSS(Cog):
         return reversed(list(itertools.takewhile(entry_is_new, feed.entries)))
 
     @tasks.loop(minutes=UPDATE_MINUTES)
-    async def rss_update_loop(self):
-        channel = await self.bot.fetch_channel(FEED_CHANNEL_ID)
+    async def rss_update_loop(self, channel_id=FEED_CHANNEL_ID):
+        channel = await self.bot.fetch_channel(channel_id)
         for feed_id, feed_url in FEEDS:
             res = feedparser.parse(feed_url)
             feed = extract_data(feed_id, res)
