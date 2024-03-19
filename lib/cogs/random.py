083bee4b4a38021cb60fb4b34224e9f4547e94fd
c-rotte <54217818+rchr1@users.noreply.github.com>
2024-03-19 16:21:32 +0100

    fixed fitstar

--- lib/cogs/random.py
+++ lib/cogs/random.py
@@ -1,5 +1,6 @@
 import binascii
 import os
+import time
 
 from discord import Embed, Colour, File
 from discord.ext.commands import Cog, command
@@ -15,6 +16,8 @@ import datetime
 import random
 
 from PIL import Image, ImageChops
+import matplotlib.pyplot as plt
+
 
 class Random(Cog):
 
@@ -40,14 +43,14 @@ class Random(Cog):
         else:
             embed = Embed(title='TAILS', color=0xca9502)
         await ctx.send(embed=embed)
-        
+
     @command()
     async def flag(self, ctx):
         """ Generate yourself a flag """
         # To remember our fallen brothers & adored tutors in the battle of the flags.
         # Never forget the brave soldiers at the front of the red flags.
         # Acta est fabula, plaudite!
-        await ctx.send('flag{' + str(binascii.b2a_hex(os.urandom(18)).decode())+'}')
+        await ctx.send('flag{' + str(binascii.b2a_hex(os.urandom(18)).decode()) + '}')
 
     @command(aliases=['orakel'])
     async def oracle(self, ctx):
@@ -78,52 +81,46 @@ class Random(Cog):
 
         embed = Embed(title=str(numbers)[1:-1].replace(',', ''), color=Colour.from_rgb(255, 255, 255))
         await ctx.send(embed=embed)
-        
+
     @command()
     async def stäbli(self, ctx):
         """ get the number of visitors in the stäblibad """
         res = requests.get("https://functions.api.ticos-systems.cloud/api/gates/counter?organizationUnitIds=30194",
-             headers={
-                 "abp-tenantid": "69"
-             })
+                           headers={
+                               "abp-tenantid": "69"
+                           })
         visitors = int(res.json()[0]['personCount'])
         max_visitors = int(res.json()[0]['maxPersonCount'])
         percentage = "{0:.0%}".format(visitors / max_visitors)
         await ctx.send(f"{visitors}/{max_visitors} ({percentage})")
-    
+
     @command()
     async def fitstar(self, ctx):
         """get the current capacity of fitstar neuried"""
-        
-        res = requests.get("https://www.mysports.com/nox/public/v1/studios/1210005340/utilization/v2/today",
-                   headers={"x-tenant": "fit-star"})
-        current_time_entry = next(filter(lambda time_entry: time_entry["current"], res.json()), None)
-        msg = "error getting data"
-        if current_time_entry:
-            percentage = current_time_entry["percentage"]
-            emojis = ["🥹", "😁", "😀", "😐", "🫤", "😒", "😠", "😤", "😡", "🤬", "💀"]
-            emoji = emojis[percentage // 10]
-        msg = f"Aktuelle Affenquote: {percentage}% {emoji}"
 
-        prediction_image_url = "http://107.173.251.156/fitstar.png"
-        actual_image_path = "/usr/share/nginx/html/fitstar_actual.png"
-        try:
-            with Image.open(actual_image_path) as actual_image:
-                
-                # Create a white background image
-                white_bg = Image.new('RGBA', actual_image.size, 'WHITE')
-                
-                # Paste the actual image onto the white background
-                white_bg.paste(actual_image, (0, 0), actual_image)
-                
-                # Save the image with white background to a byte array
-                byte_arr = io.BytesIO()
-                white_bg.save(byte_arr, format='PNG')
-                byte_arr.seek(0)
-                await ctx.send(msg, file=File(byte_arr, 'fitstar.png'))
-        except Exception as e:
-            await ctx.send(f"{msg}\n(Note: Could not load the image. {str(e)})")
-        
+        csv_path = "/home/regular/fitstar/fitstar.csv"
+        # get last 24 lines of the csv file
+        with open(csv_path, "rb") as f:
+            lines = f.readlines()[-24:]
+        # get all entries that are not older than 24 hours
+        current_timestamp = datetime.datetime.now().timestamp()
+        x = []
+        y = []
+        for line in lines:
+            timestamp, percentage = line.decode().strip().split(",")
+            if current_timestamp - float(timestamp) < 24 * 60 * 60:
+                date = datetime.datetime.fromtimestamp(float(timestamp))
+                x.append(date)
+                y.append(int(percentage))
+        # create a plot
+        plt.plot(x, y)
+        plt.xlabel("time")
+        plt.ylabel("percentage")
+        plt.title("fitstar neuried capacity")
+        plt.savefig("/tmp/fitstar.png")
+        plt.close()
+        await ctx.send(file=File("/tmp/fitstar.png"))
+
     @command()
     async def jumpers(self, ctx):
         """get the current number of npcs of jumpers"""
@@ -164,9 +161,10 @@ class Random(Cog):
             with open("/tmp/label.png", 'wb') as f:
                 f.write(response.content)
             await ctx.send(file=File("/tmp/label.png"))
-    
+
         except requests.exceptions.RequestException as e:
             await ctx.send(f'An error occurred: {e}')
+
 
 def setup(bot):
     bot.add_cog(Random(bot))
